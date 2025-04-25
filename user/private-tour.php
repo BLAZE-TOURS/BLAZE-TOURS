40aba000e681d2bfb2ee6cecc5e1e6d25814667b
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-04-26 00:56:58 +0530

    Update private-tour.php

--- user/private-tour.php
+++ user/private-tour.php
@@ -456,20 +456,43 @@ tour Area
                                         <span>Stop: 20 minutes</span>
                                     </li>
                                     <li>
-                                        <strong>You'll get picked up</strong>
-                                        <span>See departure details</span>
+                                        <strong>Galle Face Green</strong>
+                                        <span>Stop: 15 minutes</span>
                                     </li>
                                     <li>
-                                        <strong>You'll get picked up</strong>
-                                        <span>See departure details</span>
+                                        <strong>Colombo Fort Clock Tower</strong>
+                                        <span>Stop: 15 minutes</span>
                                     </li>
                                     <li>
-                                        <strong>You'll get picked up</strong>
-                                        <span>See departure details</span>
+                                        <strong>Viharamahadevi Park</strong>
+                                        <span>Stop: 30 minutes</span>
                                     </li>
                                     <li>
-                                        <strong>You'll get picked up</strong>
-                                        <span>See departure details</span>
+                                        <strong>Seema Malakaya Temple</strong>
+                                        <span>Stop: 20 minutes</span>
+                                    </li>
+                                    <li>
+                                        <strong>Colombo Lotus Tower</strong>
+                                        <span>Stop: 10 minutes - Admission excluded</span>
+                                    </li>
+                                    <li>
+                                        <strong>Pettah</strong>
+                                        <span>Stop: 30 minutes</span>
+                                    </li>
+                                    <li>
+                                        <strong>Pettah Floating Market</strong>
+                                        <span>Stop: 20 minutes</span>
+                                    </li>
+                                    <li>
+                                        <strong>Dutch Hospital Shopping Precinct</strong>
+                                        <span>Stop: 10 minutes - Admission included</span>
+                                    </li>
+                                    <li>
+                                        <strong>Cargills Department Store</strong>
+                                        <span>Stop: 5 minutes - Admission included</span>
+                                    </li>
+                                    <li>
+                                        <strong>You'll return to the starting point</strong>
                                     </li>
                                 </ul>
                             </div>
@@ -508,144 +531,7 @@ tour Area
     </section><!--==============================
 	Footer Area
 ==============================-->
-    <footer class="footer-wrapper bg-title footer-layout2">
-        <div class="widget-area">
-            <div class="container">
-                <div class="newsletter-area">
-                    <div class="newsletter-top">
-                        <div class="row gy-4 align-items-center">
-                            <div class="col-lg-5">
-                                <h2 class="newsletter-title text-white text-capitalize mb-0">get updated the latest
-                                    newsletter</h2>
-                            </div>
-                            <div class="col-lg-7">
-                                <form class="newsletter-form style2">
-                                    <input class="form-control " type="email" placeholder="Enter Email" required="">
-                                    <button type="submit" class="th-btn style1">Subscribe Now <img src="assets/img/icon/plane2.svg" alt=""></button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-between">
-                    <div class="col-md-6 col-xl-3">
-                        <div class="widget footer-widget">
-                            <div class="th-widget-about">
-                                <div class="about-logo">
-                                    <a href="home-travel.php"><img src="assets/img/logo-white.svg" alt="Tourm"></a>
-                                </div>
-                                <p class="about-text">Rapidiously myocardinate cross-platform intellectual capital model. Appropriately create interactive infrastructures</p>
-                                <div class="th-social">
-                                    <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
-                                    <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
-                                    <a href="https://www.whatsapp.com/"><i class="fab fa-whatsapp"></i></a>
-                                    <a href="https://instagram.com/"><i class="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-xl-auto">
-                        <div class="widget widget_nav_menu footer-widget">
-                            <h3 class="widget_title">Quick Links</h3>
-                            <div class="menu-all-pages-container">
-                                <ul class="menu">
-
-                                    <li><a href="index.php">Home</a></li>
-                                    <li><a href="about.php">About us</a></li>
-                                    <li><a href="service.php">Our Service</a></li>
-                                    <li><a href="contact.php">Terms of Service</a></li>
-                                    <li><a href="contact.php">Tour Booking Now</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-xl-auto">
-                        <div class="widget footer-widget">
-                            <h3 class="widget_title">Get In Touch</h3>
-                            <div class="th-widget-contact">
-                                <div class="info-box_text">
-                                    <div class="icon">
-                                        <img src="assets/img/icon/phone.svg" alt="img">
-                                    </div>
-                                    <div class="details">
-                                        <p><a href="tel:+01234567890" class="info-box_link">+01 234 567 890</a></p>
-                                        <p><a href="tel:+09876543210" class="info-box_link">+09 876 543 210</a></p>
-                                    </div>
-                                </div>
-                                <div class="info-box_text">
-                                    <div class="icon">
-                                        <img src="assets/img/icon/envelope.svg" alt="img">
-                                    </div>
-                                    <div class="details">
-                                        <p><a href="mailto:mailinfo00@tourm.com" class="info-box_link">mailinfo00@tourm.com</a></p>
-                                        <p><a href="mailto:support24@tourm.com" class="info-box_link">support24@tourm.com</a></p>
-                                    </div>
-                                </div>
-                                <div class="info-box_text">
-                                    <div class="icon"><img src="assets/img/icon/location-dot.svg" alt="img"></div>
-                                    <div class="details">
-                                        <p>789 Inner Lane, Holy park, California, USA</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-xl-auto">
-                        <div class="widget footer-widget">
-                            <h3 class="widget_title">Instagram Post</h3>
-                            <div class="sidebar-gallery">
-                                <div class="gallery-thumb">
-                                    <img src="assets/img/widget/gallery_1_1.jpg" alt="Gallery Image">
-                                    <a target="_blank" href="https://www.instagram.com/" class="gallery-btn"><i class="fab fa-instagram"></i></a>
-                                </div>
-                                <div class="gallery-thumb">
-                                    <img src="assets/img/widget/gallery_1_2.jpg" alt="Gallery Image">
-                                    <a target="_blank" href="https://www.instagram.com/" class="gallery-btn"><i class="fab fa-instagram"></i></a>
-                                </div>
-                                <div class="gallery-thumb">
-                                    <img src="assets/img/widget/gallery_1_3.jpg" alt="Gallery Image">
-                                    <a target="_blank" href="https://www.instagram.com/" class="gallery-btn"><i class="fab fa-instagram"></i></a>
-                                </div>
-                                <div class="gallery-thumb">
-                                    <img src="assets/img/widget/gallery_1_4.jpg" alt="Gallery Image">
-                                    <a target="_blank" href="https://www.instagram.com/" class="gallery-btn"><i class="fab fa-instagram"></i></a>
-                                </div>
-                                <div class="gallery-thumb">
-                                    <img src="assets/img/widget/gallery_1_5.jpg" alt="Gallery Image">
-                                    <a target="_blank" href="https://www.instagram.com/" class="gallery-btn"><i class="fab fa-instagram"></i></a>
-                                </div>
-                                <div class="gallery-thumb">
-                                    <img src="assets/img/widget/gallery_1_6.jpg" alt="Gallery Image">
-                                    <a target="_blank" href="https://www.instagram.com/" class="gallery-btn"><i class="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copyright-wrap">
-            <div class="container">
-                <div class="row justify-content-between align-items-center">
-                    <div class="col-md-6">
-                        <p class="copyright-text">Copyright 2024 <a href="home-travel.php">Tourm</a>. All Rights Reserved.</p>
-                    </div>
-                    <div class="col-md-6 text-end d-none d-md-block">
-                        <div class="footer-card">
-                            <span class="title">We Accept</span>
-                            <img src="assets/img/shape/cards.png" alt="">
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <div class="shape-mockup movingX d-none d-xxl-block" data-top="24%" data-left="5%">
-            <img src="assets/img/shape/shape_8.png" alt="shape">
-        </div>
-    </footer>
-
+<?php include 'footer.php'; ?>
     <!--********************************
 			Code End  Here 
 	******************************** -->
