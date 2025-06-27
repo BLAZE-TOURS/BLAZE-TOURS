234bf563bc1220987674f9cddd00b9cca3abd6e1
malindu prabod w.m <91063027+malinduwmp@users.noreply.github.com>
2025-06-28 00:08:54 +0530

    Update connection.php

--- admin/connection.php
+++ admin/connection.php
@@ -11,6 +11,7 @@ class Database
         if (!isset(Database::$connection)) {
 
         Database::$connection = new mysqli("localhost", "root", "#Lucky2003sql", "blaze-tours_db", 3306);
+        Database::$connection = new mysqli("localhost", "root", "SMsuperX@262", "blaze-tours_db", 3306);
         //    Database::$connection = new mysqli("localhost", "root", "SMsuperX@262", "rakkiththakanda_db", 3306, '/tmp/mysql.sock');
          //Database::$connection = new mysqli("localhost", "root", "SMsuperX@262", "rakkiththakanda_db", 3306, );
         //Database::$connection = new mysqli("localhost", "rakkitht_db", "SMsuperX@262", "rakkitht_admin_db", 3306);
