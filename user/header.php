ffd85356fe5fe7e7ff5f380cbef4684fc069173c
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-08-24 14:32:03 +0530

    remove blog

--- user/header.php
+++ user/header.php
@@ -20,7 +20,6 @@
                 </li>
                 <li><a href="about.php">About Us</a></li>
                 <li><a href="gallery.php">Gallery</a></li>
-                <li><a href="#">Blog</a></li>
                 <li><a href="contact.php">Contact us</a></li>
             </ul>
         </div>
@@ -83,7 +82,6 @@
                                 </li>
                                 <li><a href="about.php">About Us</a></li>
                                 <li><a href="gallery.php">Gallery</a></li>
-                                <li><a href="#">Blog</a></li>
                                 <li><a href="contact.php">Contact us</a></li>
                             </ul>
                         </nav>
