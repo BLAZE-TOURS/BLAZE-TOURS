51726876bfc0324212e42c9dce1faec89726f98f
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-19 20:23:07 +0530

    Update tours.php

--- user/tours.php
+++ user/tours.php
@@ -83,14 +83,6 @@
 
     </div>  -->
 
-    <div class="popup-search-box">
-        <button class="searchClose"><i class="fal fa-times"></i></button>
-        <form action="#">
-            <input type="text" placeholder="What are you looking for?">
-            <button type="submit"><i class="fal fa-search"></i></button>
-        </form>
-    </div>
-
     <!--==============================
     Header Area
 ============================== -->
