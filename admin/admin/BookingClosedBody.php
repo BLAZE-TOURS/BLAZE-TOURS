d16477859e3b7d8ef42b94796f65ede075832bbe
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-10-21 16:11:14 +0530

    Update BookingClosedBody.php

--- admin/admin/BookingClosedBody.php
+++ admin/admin/BookingClosedBody.php
@@ -94,7 +94,7 @@
                         ?>
                       </td>
                       <td>
-                        <a href="https://wa.me/<?php echo urlencode($row["mobile"]); ?>?text=Namo%20Buddhaya%20<?php echo urlencode($row["name"]); ?>,Thank%20you%20for%20booking%20the%20Meditation%20program%20at%20Rakkittakanda%20Raja%20Maha%20Viharaya."
+                        <a href="https://wa.me/<?php echo urlencode($row["mobile"]); ?>?text=Hello"
                           target="_blank"
                           class="btn btn-success btn-sm">
                           Send Message
