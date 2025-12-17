f42f561524f2c142770dfdb7050ed4e490a48bc1
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-12-17 22:51:46 +0530

    paynow

--- user/paynow/index.php
+++ user/paynow/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'assets/process/connection.php';
+require_once '../assets/process/connection.php';
 
 // Load currencies for the selector
 $currency_rs = Database::search("SELECT id, currency, country, LKR FROM currency ORDER BY id ASC");
@@ -8,6 +8,7 @@ while ($c = $currency_rs->fetch_assoc()) {
     $currencies[] = $c;
 }
 ?>
+
 <!doctype html>
 <html class="no-js" lang="zxx">
 
@@ -25,7 +26,7 @@ while ($c = $currency_rs->fetch_assoc()) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Favicons - Place favicon.ico in the root directory -->
-    <link rel="icon" type="image/png" href="assets/img/logo.png">
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
 
     <!--==============================
 	  Google Fonts
@@ -40,17 +41,17 @@ while ($c = $currency_rs->fetch_assoc()) {
 	    All CSS File
 	============================== -->
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <!-- Fontawesome Icon -->
-    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome.min.css">
     <!-- Magnific Popup -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="../assets/css/magnific-popup.min.css">
 
     <!-- Swiper css -->
-    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="../assets/css/swiper-bundle.min.css">
 
     <!-- Theme Custom CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
     <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 
@@ -71,7 +72,98 @@ while ($c = $currency_rs->fetch_assoc()) {
         </script>";
     }
     ?>
-    <?php include 'header.php'; ?>
+
+    <!-- Header -->
+    <!-- Mobile View -->
+    <div class="th-menu-wrapper onepage-nav">
+        <div class="th-menu-area text-center">
+            <button class="th-menu-toggle"><i class="fal fa-times"></i></button>
+            <div class="mobile-logo">
+                <a href="../index.php"><img src="../assets/img/logo-main.png" alt="Tour"></a>
+            </div>
+            <div class="th-mobile-menu">
+                <ul>
+                    <li class="mega-menu-wrap">
+                        <a class="active" href="../index.php">Home</a>
+                    </li>
+                    <li><a href="../tours.php">Tours</a></a></li>
+                    <li><a href="../about.php">About Us</a></li>
+                    <li><a href="../gallery.php">Gallery</a></li>
+                    <li><a href="">Paynow</a></li>
+                    <li><a href="../contact.php">Contact us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+
+    <header class="th-header header-layout1">
+        <div class="header-top">
+            <div class="container th-container">
+                <div class="row justify-content-center justify-content-xl-between align-items-center">
+                    <div class="col-auto d-none d-md-block">
+                        <div class="header-links">
+                            <ul>
+                                <li class="d-none d-xl-inline-block"><i class="fa-sharp fa-regular  fa-location-dot"></i>
+                                    <span>68/29, Sri Sidhartha road, Kirulapane, Colombo-6</span>
+                                </li>
+                                <li class="d-none d-xl-inline-block"><i class="fa-regular fa-clock text-success"></i>
+                                    <span class="text-success">Always Open</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="header-right">
+                            <div class="header-links">
+                                <ul>
+                                    <li class="d-none d-md-inline-block"><a href="../privacy-policy.php">Privacy Policy</a></li>
+                                    <li class="d-none d-md-inline-block"><a href="../terms-conditions.php">Terms & Conditions</a></li>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sticky-wrapper">
+            <!-- Main Menu Area -->
+            <div class="menu-area">
+                <div class="container th-container">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-auto">
+                            <div class="header-logo">
+                                <a href="../index.php"><img src="../assets/img/logo-main.png" alt="Tour"></a>
+                            </div>
+                        </div>
+                        <div class="col-auto me-xl-auto">
+                            <nav class="main-menu d-none d-xl-inline-block">
+                                <ul>
+                                    <li><a href="../index.php">Home</a></li>
+                                    <li><a href="../tours.php">Tours</a></a></li>
+                                    <li><a href="../about.php">About Us</a></li>
+                                    <li><a href="../gallery.php">Gallery</a></li>
+                                    <li><a href="">Paynow</a></li>
+                                    <li><a href="../contact.php">Contact us</a></li>
+                                </ul>
+                            </nav>
+                            <button type="button" class="th-menu-toggle d-block d-xl-none"><i class="far fa-bars"></i></button>
+                        </div>
+                        <div class="col-auto d-none d-xl-block">
+                            <div class="header-button">
+                                <a href="#" onclick="window.location.href='../tours.php'" class="th-btn style3 th-icon">Book Now</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="logo-bg" data-mask-src="../assets/img/logo_bg_mask.png"></div>
+            </div>
+        </div>
+    </header>
+    <!-- Header -->
 
     <div class="overflow-hidden space" id="payment-sec">
         <div class="container">
@@ -167,7 +259,7 @@ while ($c = $currency_rs->fetch_assoc()) {
     <!--==============================
 	Footer Area
 ==============================-->
-    <?php include 'footer-mini.php'; ?>
+    <?php include '../footer-mini.php'; ?>
 
 
     <!-- Scroll To Top -->
@@ -184,36 +276,36 @@ while ($c = $currency_rs->fetch_assoc()) {
 ============================== -->
     <!-- Jquery -->
     <!-- Jquery -->
-    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="../assets/js/vendor/jquery-3.6.0.min.js"></script>
     <!-- Swiper Js -->
-    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="../assets/js/swiper-bundle.min.js"></script>
     <!-- Bootstrap -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
     <!-- Magnific Popup -->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="../assets/js/jquery.magnific-popup.min.js"></script>
     <!-- Counter Up -->
-    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="../assets/js/jquery.counterup.min.js"></script>
     <!-- Range Slider -->
-    <script src="assets/js/jquery-ui.min.js"></script>
+    <script src="../assets/js/jquery-ui.min.js"></script>
     <!-- imagesloaded -->
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="../assets/js/imagesloaded.pkgd.min.js"></script>
     <!-- isotope -->
-    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="../assets/js/isotope.pkgd.min.js"></script>
     <!-- gsap -->
-    <script src="assets/js/gsap.min.js"></script>
+    <script src="../assets/js/gsap.min.js"></script>
 
     <!-- circle-progress -->
-    <script src="assets/js/circle-progress.js"></script>
+    <script src="../assets/js/circle-progress.js"></script>
 
-    <script src="assets/js/matter.min.js"></script>
-    <script src="assets/js/matterjs-custom.js"></script>
+    <script src="../assets/js/matter.min.js"></script>
+    <script src="../assets/js/matterjs-custom.js"></script>
 
 
     <!-- nice select -->
-    <script src="assets/js/nice-select.min.js"></script>
+    <script src="../assets/js/nice-select.min.js"></script>
 
     <!-- Main Js File -->
-    <script src="assets/js/main.js"></script>
+    <script src="../assets/js/main.js"></script>
 </body>
 
 </html>
