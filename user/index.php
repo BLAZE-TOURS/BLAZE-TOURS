a87d56723c374d34116499c11dab56868566c950
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-01-09 16:39:18 +0530

    Update index.php

--- user/index.php
+++ user/index.php
@@ -349,10 +349,10 @@ Service Area
                                         <div class="tour-rating">
                                             <div class="star-rating" role="img" aria-label="Rated 5.00 out of 5">
                                                 <span style="width:100%">Rated
-                                                    <strong class="rating">5.00</strong> out of 5 based on <span class="rating">4.8</span>(4.8 Rating)
+                                                    <strong class="rating">5.00</strong> out of 5 based on <span class="rating">5.0</span>(5.0 Rating)
                                                 </span>
                                             </div>
-                                            <a href="tour-page/index.php?id=<?php echo $tour['id']; ?>" class="woocommerce-review-link">(<span class="count">4.8</span> Rating)</a>
+                                            <a href="tour-page/index.php?id=<?php echo $tour['id']; ?>" class="woocommerce-review-link">(<span class="count">5.0</span> Rating)</a>
                                         </div>
                                         <h4 class="tour-box_price"><span class="currency">$<?php echo number_format($tour['adult_price'], 2); ?></span>/Person</h4>
                                         <div class="tour-action">
