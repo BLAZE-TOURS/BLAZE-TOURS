0fd9cd90bdd6ec9a404edf5c763cb44a22dec0ce
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2026-02-19 13:41:08 +0530

    Update footer.php

--- user/footer.php
+++ user/footer.php
@@ -140,13 +140,10 @@
         <div class="container">
             <div class="row justify-content-between align-items-center">
                 <div class="col-md-6">
-                    <p class="copyright-text">Copyright 2025 <a href="home-travel.php">BLAZE TOURS (PVT) LTD</a> | All Rights Reserved.</p>
+                    <p class="copyright-text">Copyright 2025 <a href="home-travel.php">BLAZE TOURS (PVT) LTD</a></p>
                 </div>
-                <div class="col-md-6 text-end d-none d-md-block">
-                    <div class="footer-card">
-                        <span class="title">We Accept</span>
-                        <a href="https://www.payhere.lk" target="_blank"><img src="assets/img/payhere.png" alt="PayHere" width="400" /></a>
-                    </div>
+                <div class="col-md-6 text-end">
+                    <p class="copyright-text">Develop and design by <a href="https://tecxone.software/" target="_blank" rel="noopener noreferrer" class="text-danger fw-bold">Tecxone Software Solution</a></p>
                 </div>
             </div>
 
